d0d71b35af01879ddddc8871026866284f5961ed
Usama Khan <usamakhandev3184@gmail.com>
2026-06-17 23:56:42 +0500

    Fix localhost URL in Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/auth'
+const API_URL = 'https://todo-backend-t8lt.onrender.com/api/auth'
 
 function Login({ onLoginSuccess, switchToRegister }) {
   const [email, setEmail] = useState('')
